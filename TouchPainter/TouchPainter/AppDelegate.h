e46e2a23a1a82a9750422ab7ca06f7274a8aaa78
cy <q2522535@163.com>
2017-09-27 23:39:44 +0800

    TouchPainter

---
+++ TouchPainter/TouchPainter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainter
+//
+//  Created by cy on 27/09/2017.
+//  Copyright © 2017 jiemu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
